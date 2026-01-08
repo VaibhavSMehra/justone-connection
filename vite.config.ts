36ed1ffa664effa277bfe3d4c4938fbf1fcb9f27
VaibhavSMehra <vaibhavmehra2027@u.northwestern.edu>
2026-01-08 16:45:01 -0600

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/justone/" : "/",
+  base: mode === "production" ? "/justone-connection/" : "/",
   server: {
     host: "::",
     port: 8080,
